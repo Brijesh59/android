4a4067a2b10827f32555f73bcf5daf7ac06f092d
Brijesh Kumar <kumarbrijesh9021@gmail.com>
2018-10-12 21:56:16 +0530

    Create MainActivity.java

---
+++ Android-Projects/Intent/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.infinity.intentexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.submitBtn);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                // putExtra("key", "value");
+                intent.putExtra("name","Disha");
+                intent.putExtra("marks", 100);
+                startActivity(intent);
+            }
+        });
+    }
+}
