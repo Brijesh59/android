601f93fc1023679478340c67b271d07eff5fb9a2
Brijesh Kumar <kumarbrijesh9021@gmail.com>
2018-11-08 11:12:54 +0530

    Update AlertDialog.java

--- Android-Projects/AlertDialog/AlertDialog.java
+++ Android-Projects/AlertDialog/AlertDialog.java
@@ -3,16 +3,16 @@ builder.setTitle("Your Title");
 builder.setMessage("Your Message");
 builder.setCancelable(true);
 
-builder1.setPositiveButton( "Yes", new DialogInterface.OnClickListener() {
+builder.setPositiveButton( "Yes", new DialogInterface.OnClickListener() {
   public void onClick(DialogInterface dialog, int id) {
     // do what so ever
   }
 });
 
-builder1.setNegativeButton( "No", new DialogInterface.OnClickListener() {
+builder.setNegativeButton( "No", new DialogInterface.OnClickListener() {
   public void onClick(DialogInterface dialog, int id) {
     dialog.cancel();
   }
 });
 
-builder1.create().show();
+builder.create().show();
